a3142573d1125a82d3a14f6827b700e8f5dbf61a
Hugh Kaznowski <phkaznowski@gmail.com>
2023-11-16 23:43:51 +0000

    Add whitespace test

--- parser/src/keyword/constant.rs
+++ parser/src/keyword/constant.rs
@@ -1,5 +1,6 @@
 use nom::branch::alt;
-use nom::bytes::complete::tag;
+use nom::bytes::complete::{tag, };
+use nom::character::complete::multispace0;
 use nom::IResult;
 
 #[derive(Debug, PartialEq)]
@@ -8,8 +9,9 @@ pub struct ConstantDeclarations {
 }
 
 pub fn constant(input: &str) -> IResult<&str, ConstantDeclarations> {
-    let (remainder, _) = alt((tag("CONSTANT"), tag("CONSTANTS")))(input)?;
-    Ok((remainder, ConstantDeclarations{ constants: vec![] }))
+    let (input, _) = multispace0(input)?;
+    let (input, _) = alt((tag("CONSTANT"), tag("CONSTANTS")))(input)?;
+    Ok((input, ConstantDeclarations{ constants: vec![] }))
 }
 
 #[cfg(test)]
@@ -17,30 +19,23 @@ mod test {
     use crate::keyword::constant::{constant, ConstantDeclarations};
 
     #[test]
-    pub fn singular_empty() {
-        let res = constant("CONSTANT");
-        assert!(res.is_ok());
-        let (remainder, const_declaration) = res.unwrap();
-        assert_eq!(remainder, "");
-        assert_eq!(const_declaration, ConstantDeclarations{ constants: vec![] })
+    pub fn singular_valid() {
+        let cases = [
+            ("CONSTANT", ("", ConstantDeclarations{ constants: vec![] })),
+            (" CONSTANT", ("", ConstantDeclarations{ constants: vec![] })),
+        ];
+        for (index, (input, output)) in cases.iter().enumerate() {
+            println!("Case {}: input={:?}, output={:?}", index, input, output);
+            let res = constant(input);
+            assert!(res.is_ok());
+            let (remainder, const_declaration) = res.unwrap();
+            assert_eq!(remainder, output.0);
+            assert_eq!(const_declaration, output.1);
+        }
     }
 
     #[test]
-    pub fn singular_single() {
-    }
-
-    #[test]
-    pub fn singular_many() {
-
-    }
-
-    #[test]
-    pub fn plural_single() {
-
-    }
-
-    #[test]
-    pub fn plural_many() {
+    pub fn plural_valid() {
 
     }
 }
