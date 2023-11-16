c70aecaae313071d757ac985a28f7b962cdfee0e
Hugh Kaznowski <phkaznowski@gmail.com>
2023-11-16 22:51:16 +0000

    Add module declaration

--- parser/src/model.rs
+++ parser/src/model.rs
@@ -5,6 +5,7 @@ pub struct TlaSpec {
     pub define_block: DefineBlock,
     pub variable_block: VariableBlock,
     pub invariant_block: InvariantBlock,
+    pub theorem_block: TheoremBlock,
 }
 
 pub struct ModuleBlock {
@@ -12,7 +13,12 @@ pub struct ModuleBlock {
 }
 
 pub struct ExtendsBlock {
+    pub modules: Vec<ExtendModuleDeclaration>,
+}
 
+pub struct ExtendModuleDeclaration {
+    pub module_name: String,
+    pub operators: Vec<OperatorDefinition>,
 }
 
 pub struct DefineBlock {
@@ -27,7 +33,15 @@ pub struct VariableBlock {
 
 }
 
+pub struct TheoremBlock {
+
+}
+
+pub struct OperatorDefinition {
+
+}
+
 /// Syntax: Op(arg1, arg2) == Expr
 pub struct Operator {
-
+    pub definition: OperatorDefinition,
 }
