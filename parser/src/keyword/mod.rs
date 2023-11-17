d3d3dc00649d06675621b37ec8bd1146e8c3ee4c
Hugh Kaznowski <phkaznowski@gmail.com>
2023-11-17 19:10:33 +0000

    Add variable

--- parser/src/keyword/mod.rs
+++ parser/src/keyword/mod.rs
@@ -1,1 +1,2 @@
 mod constant;
+mod variable;
--- parser/src/keyword/variable.rs
+++ parser/src/keyword/variable.rs
@@ -6,36 +6,36 @@ use nom::multi::separated_list1;
 use nom::sequence::preceded;
 
 #[derive(Debug, PartialEq)]
-pub struct ConstantDeclarations<'a> {
-    pub constants: Vec<&'a str>,
+pub struct VariableDeclarations<'a> {
+    pub variables: Vec<&'a str>,
 }
 
-pub fn constant(input: &str) -> IResult<&str, ConstantDeclarations> {
+pub fn variable(input: &str) -> IResult<&str, VariableDeclarations> {
     let (input, _) = multispace0(input)?;
-    let (input, _) = alt((tag("CONSTANTS"), tag("CONSTANT")))(input)?;
+    let (input, _) = alt((tag("VARIABLES"), tag("VARIABLE")))(input)?;
     let (input, _) = multispace1(input)?;
     let (input, vars) = separated_list1( char(','), preceded(multispace0, alpha1))(input)?;
 
-    Ok((input, ConstantDeclarations{ constants: vars }))
+    Ok((input, VariableDeclarations { variables: vars }))
 }
 
 #[cfg(test)]
 mod test {
-    use crate::keyword::constant::{constant, ConstantDeclarations};
+    use crate::keyword::variable::{variable, VariableDeclarations};
 
     #[test]
     pub fn valid() {
         let cases = [
-            ("CONSTANT a", ("", ConstantDeclarations{ constants: vec!["a"] })),
-            (" \t CONSTANT b", ("", ConstantDeclarations{ constants: vec!["b"] })),
-            ("CONSTANTS c", ("", ConstantDeclarations{ constants: vec!["c"] })),
-            (" \t CONSTANTS d", ("", ConstantDeclarations{ constants: vec!["d"] })),
-            ("CONSTANT a, e", ("", ConstantDeclarations{ constants: vec!["a", "e"] })),
-            ("CONSTANT a, e, ", (", ", ConstantDeclarations{ constants: vec!["a", "e"] })),
+            ("VARIABLE a", ("", VariableDeclarations { variables: vec!["a"] })),
+            (" \t VARIABLE b", ("", VariableDeclarations { variables: vec!["b"] })),
+            ("VARIABLES c", ("", VariableDeclarations { variables: vec!["c"] })),
+            (" \t VARIABLES d", ("", VariableDeclarations { variables: vec!["d"] })),
+            ("VARIABLE a, e", ("", VariableDeclarations { variables: vec!["a", "e"] })),
+            ("VARIABLE a, e, ", (", ", VariableDeclarations { variables: vec!["a", "e"] })),
         ];
         for (index, (input, output)) in cases.iter().enumerate() {
             println!("Case {}: input={:?}, output={:?}", index, input, output);
-            let res = constant(input);
+            let res = variable(input);
             assert!(res.is_ok());
             let (remainder, const_declaration) = res.unwrap();
             assert_eq!(remainder, output.0);
