8edea426f3a403ebf0180d2532267e11e4b85527
Hugh Kaznowski <phkaznowski@gmail.com>
2023-11-17 00:17:27 +0000

    Add list variables

--- parser/src/keyword/constant.rs
+++ parser/src/keyword/constant.rs
@@ -1,17 +1,27 @@
 use nom::branch::alt;
 use nom::bytes::complete::{tag, };
-use nom::character::complete::multispace0;
+use nom::character::complete::{alpha0, alpha1, char, multispace0, multispace1};
 use nom::IResult;
+use nom::multi::separated_list1;
+use nom::sequence::preceded;
 
 #[derive(Debug, PartialEq)]
-pub struct ConstantDeclarations {
-    pub constants: Vec<String>,
+pub struct ConstantDeclarations<'a> {
+    pub constants: Vec<&'a str>,
+}
+
+pub fn constant_entry(input: &str) -> IResult<&str, String> {
+    let (input, name) = alpha0(input)?;
+    Ok((input, name.to_string()))
 }
 
 pub fn constant(input: &str) -> IResult<&str, ConstantDeclarations> {
     let (input, _) = multispace0(input)?;
-    let (input, _) = alt((tag("CONSTANT"), tag("CONSTANTS")))(input)?;
-    Ok((input, ConstantDeclarations{ constants: vec![] }))
+    let (input, _) = alt((tag("CONSTANTS"), tag("CONSTANT")))(input)?;
+    let (input, _) = multispace1(input)?;
+    let (input, vars) = separated_list1( char(','), preceded(multispace0, alpha1))(input)?;
+
+    Ok((input, ConstantDeclarations{ constants: vars }))
 }
 
 #[cfg(test)]
@@ -19,10 +29,14 @@ mod test {
     use crate::keyword::constant::{constant, ConstantDeclarations};
 
     #[test]
-    pub fn singular_valid() {
+    pub fn valid() {
         let cases = [
-            ("CONSTANT", ("", ConstantDeclarations{ constants: vec![] })),
-            (" CONSTANT", ("", ConstantDeclarations{ constants: vec![] })),
+            ("CONSTANT a", ("", ConstantDeclarations{ constants: vec!["a"] })),
+            (" \t CONSTANT b", ("", ConstantDeclarations{ constants: vec!["b"] })),
+            ("CONSTANTS c", ("", ConstantDeclarations{ constants: vec!["c"] })),
+            (" \t CONSTANTS d", ("", ConstantDeclarations{ constants: vec!["d"] })),
+            ("CONSTANT a, e", ("", ConstantDeclarations{ constants: vec!["a", "e"] })),
+            ("CONSTANT a, e, ", (", ", ConstantDeclarations{ constants: vec!["a", "e"] })),
         ];
         for (index, (input, output)) in cases.iter().enumerate() {
             println!("Case {}: input={:?}, output={:?}", index, input, output);
@@ -35,7 +49,7 @@ mod test {
     }
 
     #[test]
-    pub fn plural_valid() {
+    pub fn invalid() {
 
     }
 }
